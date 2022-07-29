e478b33792087a9bcb64f765a3de95b082d8e18f
DesyNurfitriani <102567062+DesyNurfitriani@users.noreply.github.com>
2022-07-29 22:22:09 +0700

    Add files via upload

---
+++ kasir.com/application/views/partials/head.php
@@ -0,0 +1,6 @@
+<!-- Font Awesome -->
+<link rel="stylesheet" href="<?php echo base_url('assets/vendor/adminlte/') ?>plugins/fontawesome-free/css/all.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="	<?php echo base_url('assets/vendor/adminlte/') ?>dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="css/app-light.css" id="lightTheme">
+    <link rel="stylesheet" href="css/app-dark.css" id="darkTheme" disabled>
